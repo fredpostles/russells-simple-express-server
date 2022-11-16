9bec647ddba26cc444a6c054e1e73b53468e6a5c
Your Name <fred.postles@gmail.com>
2022-11-16 15:36:54 +0000

    Fixed Update route to work with SQL prepared statements

--- routes/update.js
+++ routes/update.js
@@ -7,24 +7,26 @@ router.put("/", async (req, res) => {
   let { email, name, password } = req.body;
   const { token } = req.headers;
 
-  const query = updateUser();
   let params = [];
 
   if (email && typeof email === "string") {
     let column = "email";
+    const query = updateUser(column);
     params = [email, token];
-    await req.asyncMySQL(query, params, column);
+    await req.asyncMySQL(query, params);
   }
   if (name && typeof name === "string") {
     let column = "name";
+    const query = updateUser(column);
     params = [name, token];
-    await req.asyncMySQL(query, params, column);
+    await req.asyncMySQL(query, params);
   }
   if (password && typeof password === "string") {
     password = sha256(process.env.SALT + password);
     let column = "password";
+    const query = updateUser(column);
     params = [password, token];
-    await req.asyncMySQL(query, params, column);
+    await req.asyncMySQL(query, params);
   }
 
   res.send({ status: 1 });
